d267f982e113eb0c73269d2ddd675f8963744caa
Ruben Bridgewater <ruben@bridgewater.de>
2018-09-13 20:19:57 +0200

    Add basic tests

---
+++ test/basic.test.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const { test } = require('tap')
+const rimraf = require('rimraf')
+const ClinicFlame = require('../index.js')
+
+test('cmd - test collect - data exists', function (t) {
+  const tool = new ClinicFlame()
+
+  function cleanup (err, dirname) {
+    t.ifError(err)
+
+    let count = 0
+    function callback (err) {
+      t.ifError(err)
+      if (++count === 2) {
+        t.end()
+      }
+    }
+    rimraf(dirname, callback)
+    fs.unlink(dirname + '.html', callback)
+  }
+
+  tool.collect(
+    [process.execPath, path.join('test', 'fixtures', 'inspect.js')],
+    function (err, dirname) {
+      if (err) return cleanup(err, dirname)
+
+      tool.visualize(dirname, dirname + '.html', function (err) {
+        if (err) return cleanup(err, dirname)
+
+        fs.readFile(dirname + '.html', function (err, content) {
+          if (err) return cleanup(err, dirname)
+
+          t.ok(content.length > 1024 * 100)
+          cleanup(null, dirname)
+        })
+      })
+    }
+  )
+})
+
+test('cmd - test visualization - missing data', function (t) {
+  const tool = new ClinicFlame({ debug: true })
+
+  tool.visualize(
+    'missing.clinic-flame',
+    'missing.clinic-flame.html',
+    function (err) {
+      // The message is set by 0x.
+      t.match(err.message, /Invalid data path provided/)
+      t.end()
+    }
+  )
+})
