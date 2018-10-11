0a320dc0a0a1a35b3839abea49629bf30291376e
Renée Kooi <renee@kooi.me>
2018-10-11 16:59:05 +0200

    Add area key (#20)
    * Add CSS variables for code area colours.
    
    * Add Area Key elements.
    
    * Minimum distance between key legends.
    
    * Realign legend colour blocks

--- visualizer/area-key.js
+++ visualizer/area-key.js
@@ -10,6 +10,21 @@ class AreaKey extends HtmlContent {
     contentProperties = Object.assign({}, defaults, contentProperties)
     super(parentContent, contentProperties)
   }
+
+  initializeElements () {
+    super.initializeElements()
+
+    this.d3Element.append('div')
+      .classed('key key-app', true)
+      // TODO Set this to the actual app name inside a setData() method once the analysis contains it.
+      .text('app')
+    this.d3Element.append('div')
+      .classed('key key-deps', true)
+      .text('dependencies')
+    this.d3Element.append('div')
+      .classed('key key-core', true)
+      .text('node core')
+  }
 }
 
 module.exports = AreaKey
