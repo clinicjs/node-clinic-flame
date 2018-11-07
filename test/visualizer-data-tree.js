b0709147162cdb2ad4b154f48b42ef40864d2a9e
Renée Kooi <renee@kooi.me>
2018-11-07 14:34:08 +0100

    Group by individual dependencies

--- test/visualizer-data-tree.js
+++ test/visualizer-data-tree.js
@@ -10,13 +10,13 @@ test('visualizer - data tree - sort frames by code area and stack top value', (t
     // Fastify totals 260
     // → fastify should be first
     children: [
-      { type: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
-      { type: 'core', value: 200, onStackTop: { base: 200 }, children: [] },
-      { type: 'fastify', value: 20, onStackTop: { base: 20 }, children: [] },
-      { type: 'app', value: 115, onStackTop: { base: 115 }, children: [] },
-      { type: 'app', value: 116, onStackTop: { base: 116 }, children: [] },
-      { type: 'fastify', value: 40, onStackTop: { base: 40 }, children: [] },
-      { type: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] }
+      { category: 'deps', typeTEMP: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
+      { category: 'all-core', type: 'core', value: 200, onStackTop: { base: 200 }, children: [] },
+      { category: 'deps', typeTEMP: 'fastify', value: 20, onStackTop: { base: 20 }, children: [] },
+      { category: 'app', typeTEMP: '.', value: 115, onStackTop: { base: 115 }, children: [] },
+      { category: 'app', typeTEMP: '.', value: 116, onStackTop: { base: 116 }, children: [] },
+      { category: 'deps', typeTEMP: 'fastify', value: 40, onStackTop: { base: 40 }, children: [] },
+      { category: 'deps', typeTEMP: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] }
     ]
   }
 
@@ -34,13 +34,13 @@ test('visualizer - data tree - sort frames by code area and stack top value', (t
   // Use match() rather than same(), because DataTree adds some properties
   // like `onStackTop.asViewed` that are not relevant for this test
   t.match(sorted, [
-    { type: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
-    { type: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
-    { type: 'fastify', value: 40, onStackTop: { base: 40 }, children: [] },
-    { type: 'fastify', value: 20, onStackTop: { base: 20 }, children: [] },
-    { type: 'app', value: 116, onStackTop: { base: 116 }, children: [] },
-    { type: 'app', value: 115, onStackTop: { base: 115 }, children: [] },
-    { type: 'core', value: 200, onStackTop: { base: 200 }, children: [] }
+    { category: 'deps', typeTEMP: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
+    { category: 'deps', typeTEMP: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
+    { category: 'deps', typeTEMP: 'fastify', value: 40, onStackTop: { base: 40 }, children: [] },
+    { category: 'deps', typeTEMP: 'fastify', value: 20, onStackTop: { base: 20 }, children: [] },
+    { category: 'app', typeTEMP: '.', value: 116, onStackTop: { base: 116 }, children: [] },
+    { category: 'app', typeTEMP: '.', value: 115, onStackTop: { base: 115 }, children: [] },
+    { category: 'all-core', type: 'core', value: 200, onStackTop: { base: 200 }, children: [] }
   ])
 
   tree.exclude.add('app')
@@ -50,16 +50,16 @@ test('visualizer - data tree - sort frames by code area and stack top value', (t
     .sort(tree.getFilteredStackSorter())
 
   t.match(sorted2, [
-    { type: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
-    { type: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
-    { type: 'fastify', value: 40, onStackTop: { base: 40 }, children: [] },
-    { type: 'fastify', value: 20, onStackTop: { base: 20 }, children: [] },
-    { type: 'core', value: 200, onStackTop: { base: 200 }, children: [] },
+    { category: 'deps', typeTEMP: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
+    { category: 'deps', typeTEMP: 'fastify', value: 100, onStackTop: { base: 100 }, children: [] },
+    { category: 'deps', typeTEMP: 'fastify', value: 40, onStackTop: { base: 40 }, children: [] },
+    { category: 'deps', typeTEMP: 'fastify', value: 20, onStackTop: { base: 20 }, children: [] },
+    { category: 'all-core', type: 'core', value: 200, onStackTop: { base: 200 }, children: [] },
     // NOTE these are not sorted by value—they don't need to be
     // If this test starts failing we should change the test so it doesn't rely on this order
     // (or change the implementation so it sorts hidden frames by _something_)
-    { type: 'app', value: 115, onStackTop: { base: 115 }, children: [] },
-    { type: 'app', value: 116, onStackTop: { base: 116 }, children: [] }
+    { category: 'app', typeTEMP: '.', value: 115, onStackTop: { base: 115 }, children: [] },
+    { category: 'app', typeTEMP: '.', value: 116, onStackTop: { base: 116 }, children: [] }
   ])
 
   t.end()
