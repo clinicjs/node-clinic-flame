c15edbdf9465dc78cb54f345babafbcff04a493c
AlanSl <alan.slater@nearform.com>
2018-11-19 07:11:52 +0000

    Widen presentation mode heat bars

--- visualizer/flame-graph-frame.js
+++ visualizer/flame-graph-frame.js
@@ -25,6 +25,7 @@ function renderStackFrame (globals, locals, rect) {
   const thick = 3
   const thin = 1
   const lineWidth = this.ui.presentationMode ? thick : thin
+  const heatHeight = Math.ceil(rect.height / (this.ui.presentationMode ? 2.5 : 3))
 
   // Do nothing for frames excluded by zoom, unless we're still animating
   if (nodeData.value === 0 && !this.isAnimating) return
@@ -40,13 +41,15 @@ function renderStackFrame (globals, locals, rect) {
   if (width <= 1.5) {
     const backgroundColor = this.ui.getFrameColor(nodeData, 'background', false)
     const foregroundColor = this.ui.getFrameColor(nodeData, 'foreground', false)
-    renderAsLine(context, { x: left, y, height }, backgroundColor, foregroundColor, nodeData.highlight)
+    renderAsLine(context, { x: left, y, height }, backgroundColor, foregroundColor, nodeData.highlight, heatHeight)
     return
   }
 
   // Don't redraw heat over previous paint on hover events, and don't draw for root node
   // if (state === STATE_IDLE && nodeData.id !== 0) renderHeatBar(context, nodeData, colorHash, alignedRect)
-  if (state === STATE_IDLE && nodeData.id !== 0) renderHeatBar(context, nodeData, colorHash, rect)
+  if (state === STATE_IDLE && nodeData.id !== 0) {
+    renderHeatBar(context, nodeData, colorHash, rect, heatHeight)
+  }
 
   const backgroundColor = this.ui.getFrameColor(nodeData, 'background')
   const foregroundColor = this.ui.getFrameColor(nodeData, 'foreground')
@@ -82,11 +85,10 @@ function renderStackFrame (globals, locals, rect) {
   context.restore()
 }
 
-function renderHeatBar (context, nodeData, colorHash, rect) {
+function renderHeatBar (context, nodeData, colorHash, rect, heatHeight) {
   // Extracted from d3-fg so we can pixel-align it to match the rest
   const heatColor = colorHash(nodeData)
   const heatStrokeColor = colorHash(nodeData, 1.1)
-  const heatHeight = getHeatHeight(rect.height)
 
   context.fillStyle = heatColor
   context.strokeStyle = heatStrokeColor
@@ -96,7 +98,7 @@ function renderHeatBar (context, nodeData, colorHash, rect) {
   context.stroke()
 }
 
-function renderAsLine (context, rect, backgroundColor, foregroundColor, isHighlighted) {
+function renderAsLine (context, rect, backgroundColor, foregroundColor, isHighlighted, heatHeight) {
   const {
     x,
     y,
@@ -106,7 +108,7 @@ function renderAsLine (context, rect, backgroundColor, foregroundColor, isHighli
   // Black solid background line, including black heat area
   context.strokeStyle = backgroundColor
   context.beginPath()
-  context.moveTo(x, y - getHeatHeight(height))
+  context.moveTo(x, y - heatHeight)
   context.lineTo(x, y + height)
   context.stroke()
 
@@ -121,10 +123,6 @@ function renderAsLine (context, rect, backgroundColor, foregroundColor, isHighli
   context.lineTo(x, y + height)
   context.stroke()
   context.restore()
-}
-
-function getHeatHeight (height) {
-  return Math.ceil(height / 3)
 }
 
 module.exports = getFrameRenderer
