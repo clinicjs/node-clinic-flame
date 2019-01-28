3a3510c1c1b6b04287e27e87ffce09472eca0928
AlanSl <alan.slater@nearform.com>
2019-01-28 08:45:28 +0000

    Remove gaps between area change horizontal lines

--- visualizer/flame-graph-frame.js
+++ visualizer/flame-graph-frame.js
@@ -93,10 +93,11 @@ function renderStackFrame (globals, locals, rect) {
   const alphaReduced = this.ui.presentationMode ? 0.25 : 0.2
 
   const areaChangeBelow = !sameArea(nodeData, visibleParent.data)
-
   const areaChangeLeft = !previousSibling || !sameArea(nodeData, previousSibling.data)
   const areaChangeRight = !nextSibling || !sameArea(nodeData, nextSibling.data)
   const categoryChangeBelow = nodeData.category !== visibleParent.data.category
+
+  const leftGapRemover = !areaChangeLeft && areaChangeBelow ? 1 : 0
 
   context.globalAlpha = (areaChangeBelow ? alphaFull : alphaReduced) * thinFrameReducer
 
@@ -105,7 +106,7 @@ function renderStackFrame (globals, locals, rect) {
     context.strokeStyle = parentForegroundColor
 
     context.beginPath()
-    context.moveTo(left, bottom - lineWidth * 1.5)
+    context.moveTo(left - leftGapRemover, bottom - lineWidth * 1.5)
     context.lineTo(right, bottom - lineWidth * 1.5)
     context.stroke()
   }
@@ -113,7 +114,7 @@ function renderStackFrame (globals, locals, rect) {
   context.strokeStyle = foregroundColor
 
   context.beginPath()
-  context.moveTo(left, bottom - lineWidth * (categoryChangeBelow ? 2.5 : 1))
+  context.moveTo(left - leftGapRemover, bottom - lineWidth * (categoryChangeBelow ? 2.5 : 1))
   context.lineTo(right, bottom - lineWidth * (categoryChangeBelow ? 2.5 : 1))
   context.stroke()
 
