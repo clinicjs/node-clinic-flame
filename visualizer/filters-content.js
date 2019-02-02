4075502cbb3830275e0a2aa2e024add2a7e5fce0
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-02-02 23:02:45 +0100

    Cleaning code and fixing Advanced section

--- visualizer/filters-content.js
+++ visualizer/filters-content.js
@@ -56,26 +56,36 @@ class FiltersContent extends HtmlContent {
 
       // *  *  *  * Code Areas *  *  *  *
       this.sections.codeAreas = codeAreas.map(area => {
+        const disabled = data.excludeKey === 'deps'
+          // check Dependencies only
+          ? data.count === 0
+          : data.disabled === true
+
+        const checked = (() => {
+          if (area.children && area.children.length) {
+            return area.children.some((child) => !exclude.has(child.excludeKey))
+          }
+          return !exclude.has(area.excludeKey)
+        })()
+
+        const indeterminate = (() => {
+          const { children } = area
+          if (!Array.isArray(children) || children.length === 0) {
+            return false
+          }
+
+          const first = exclude.has(children[0].excludeKey)
+          return children.some((child) => exclude.has(child.excludeKey) !== first)
+        })()
+
         return {
           ...area,
           count: this.getDataCountFromKey(area.excludeKey),
           label: this.ui.getLabelFromKey(area.excludeKey),
           description: this.ui.getDescriptionFromKey(area.excludeKey),
-          checked: (() => {
-            if (area.children && area.children.length) {
-              return area.children.some((child) => !exclude.has(child.excludeKey))
-            }
-            return !exclude.has(area.excludeKey)
-          })(),
-          indeterminate: (() => {
-            const { children } = area
-            if (!Array.isArray(children) || children.length === 0) {
-              return false
-            }
-
-            const first = exclude.has(children[0].excludeKey)
-            return children.some((child) => exclude.has(child.excludeKey) !== first)
-          })(),
+          disabled,
+          checked,
+          indeterminate,
           onChange: (datum, i, nodes) => {
             onVisibilityChange(datum, i, nodes, this.ui)
           }
@@ -83,10 +93,38 @@ class FiltersContent extends HtmlContent {
       })
 
       // *  *  *  * Advanced *  *  *  *
-      this.sections.advanced = {
-        useMerged,
-        showOptimizationStatus
-      }
+      this.sections.advanced = [
+        {
+          id: 'option-init',
+          label: 'Init',
+          description: 'Show initialization operations hidden by default, like module loading',
+          checked: !exclude.has('is:init'),
+          onChange: (datum, i, nodes) => {
+            this.ui.setCodeAreaVisibility('is:init', nodes[i].checked)
+            this.ui.draw()
+          }
+        },
+        {
+          id: 'option-usemergedtree',
+          label: 'Merge',
+          description: 'Join optimized and unoptimized versions of frames',
+          checked: useMerged,
+          onChange: (datum, i, nodes) => {
+            this.ui.setUseMergedTree(nodes[i].checked)
+          }
+        },
+        {
+          id: 'option-showoptimizationstatus',
+          label: 'Show optimization status',
+          description: 'Highlight frames that are optimized functions',
+          disabled: useMerged,
+          checked: showOptimizationStatus,
+          onChange: (datum, i, nodes) => {
+            this.ui.setShowOptimizationStatus(nodes[i].checked)
+          }
+        }
+
+      ]
 
       this.exclude = exclude
     }
@@ -94,18 +132,21 @@ class FiltersContent extends HtmlContent {
 
   initializeElements () {
     super.initializeElements()
-
     // creating the main sections
+
+    // *  *  *  * Code Areas *  *  *  *
     this.d3CodeArea = this.d3ContentWrapper.append('div')
       .classed('code-area section', true)
     this.d3CodeArea.append('h2').text('Visibility by code area')
     this.d3CodeArea.append('ul').classed('options', true)
 
+    // *  *  *  * Advanced *  *  *  *
     this.d3Advanced = this.d3ContentWrapper.append('div')
       .classed('advanced section', true)
     this.d3Advanced.append('h2').text('Advanced')
     this.d3Advanced.append('ul').classed('options', true)
 
+    // *  *  *  * Preferences *  *  *  *
     this.d3Preferences = this.d3ContentWrapper.append('div')
       .classed('preferences section', true)
     this.d3Preferences.append('h2').text('Preferences')
@@ -121,18 +162,14 @@ class FiltersContent extends HtmlContent {
 
     if (this.sections) {
       // *  *  *  * Code Areas *  *  *  *
-      let dataSelection = this.d3CodeArea.select('ul').selectAll('li').data(this.sections.codeAreas)
-      dataSelection.exit().remove()
-      dataSelection.call(updateOptionElement)
+      let d3NewLi = createListItems(this.d3CodeArea.select('ul'), this.sections.codeAreas)
 
-      const d3Li = dataSelection.enter().append('li').call(createOptionElementSL)
-
-      const d3SubUl = d3Li.merge(dataSelection).selectAll('ul').data(d => d.children ? [d.children] : [])
+      const d3SubUl = d3NewLi.selectAll('ul').data(d => d.children ? [d.children] : [])
       d3SubUl.exit().remove()
       const newUl = d3SubUl.enter().append('ul')
 
       // subfilters
-      dataSelection = d3SubUl.merge(newUl).selectAll('li').data(data => data.map(d => {
+      let dataSelection = d3SubUl.merge(newUl).selectAll('li').data(data => data.map(d => {
         d.label = this.ui.getLabelFromKey(d.excludeKey)
         d.description = this.ui.getDescriptionFromKey(d.excludeKey)
         d.checked = (() => {
@@ -147,18 +184,16 @@ class FiltersContent extends HtmlContent {
 
         return d
       }))
-      dataSelection.enter().append('li').call(createOptionElementSL)
+      dataSelection.enter().append('li')
+        .call(createOptionElementSL)
+        .call(updateOptionElement)
       dataSelection.exit().remove()
 
       // *  *  *  * Advanced *  *  *  *
-      dataSelection = this.d3Advanced.select('ul').selectAll('li').data(this.sections.advanced)
-      dataSelection.enter().append('li').call(createOptionElementSL)
-      dataSelection.exit().remove()
+      createListItems(this.d3Advanced.select('ul'), this.sections.advanced)
 
       // *  *  *  * Preferences *  *  *  *
-      dataSelection = this.d3Preferences.select('ul').selectAll('li').data(this.sections.preferences)
-      dataSelection.enter().append('li').call(createOptionElementSL)
-      dataSelection.exit().remove()
+      createListItems(this.d3Preferences.select('ul'), this.sections.preferences)
     }
   }
 }
@@ -183,31 +218,31 @@ function onVisibilityChange (datum, i, nodes, ui) {
   ui.draw()
 }
 
+function createListItems (ul, data) {
+  const dataSelection = ul.selectAll('li').data(data)
+  dataSelection.exit().remove()
+  const d3NewLi = dataSelection.enter().append('li').call(createOptionElementSL)
+  return d3NewLi.merge(dataSelection).call(updateOptionElement)
+}
+
 function updateOptionElement (li) {
-  li.select('input')
+  li
+    .classed('visible', d => d.visible === true)
+    .classed('childrenVisibilityToggle', d => d.childrenVisibilityToggle === true)
+    .classed('disabled', (data) => data.disabled === true)
+
+    .select('input')
+    .attr('disabled', d => d.disabled || null)
     .property('checked', d => d.checked)
     .property('indeterminate', (d) => d.indeterminate)
+    .property('class', d => d.disabled ? 'disabled' : '')
 }
 
 function createOptionElementSL (li) {
-  li.classed('visible', d => d.visible === true)
-  li.classed('childrenVisibilityToggle', d => d.childrenVisibilityToggle === true)
-  li.classed('disabled', (d) => {
-    // check Dependencies only
-    if (d.excludeKey === 'deps') {
-      return d.count === 0
-    }
-    return false
-  })
-
   li.html(data => `
-    <div class='${data.excludeKey && data.excludeKey.split(':')[0]}'>
+    <div class="${data.excludeKey ? data.excludeKey.split(':')[0] : ''}">
       <label>
-        <input type="checkbox" 
-          ${data.checked ? 'checked' : ''}
-          ${data.indeterminate ? 'indeterminate' : ''}
-          ${(data.excludeKey !== 'deps') ? (data.count === 0 ? true : '') : ''}
-        >
+        <input type="checkbox" />
         <span class="icon-wrapper">
           ${checkboxCheckedIcon}
           ${checkboxUncheckedIcon}
