f9952fbb9ac8ed4e01224e4f515efb09fd18ebfd
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-02-11 10:32:13 +0100

    moving common under visualizer

--- visualizer/common/check-button-combo.js
+++ visualizer/common/check-button-combo.js
@@ -5,17 +5,15 @@ const checkbox = require('./checkbox.js')
 module.exports = ({ label, classes = [], buttonIcon = caretUp, disabled = false, indeterminate = false } = {}) => {
   const wrapper = document.createElement('div')
   wrapper.classList.add('combo-wrapper', ...classes)
-  const comboButton = button({
-    disabled,
-    leftIcon: buttonIcon
-  })
-  const comboCheckBox = checkbox({
+
+  wrapper.appendChild(checkbox({
     leftLabel: label,
     indeterminate
-  })
-
-  wrapper.appendChild(comboCheckBox)
-  wrapper.appendChild(comboButton)
+  }))
+  wrapper.appendChild(button({
+    disabled,
+    leftIcon: buttonIcon
+  }))
 
   return wrapper
 }
