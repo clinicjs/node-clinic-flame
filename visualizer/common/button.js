700a928e697cae8f4e7b193e8eb22c3fd34d6141
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-02-18 11:25:20 +0100

    weaking button css and onclick function

--- visualizer/common/button.js
+++ visualizer/common/button.js
@@ -1,8 +1,11 @@
-module.exports = ({ label, classes = [], leftIcon = '', rightIcon = '', disabled = false } = {}) => {
+module.exports = ({ label, classes = [], leftIcon = '', rightIcon = '', disabled = false, onClick } = {}) => {
   const button = document.createElement('button')
   button.classList.add('button', ...classes)
   if (disabled) button.attr('disabled', true)
 
+  if (onClick) {
+    button.addEventListener('click', onClick)
+  }
   button.innerHTML = `
     ${leftIcon}
     ${label ? `<span class="label">${label}</span>` : ``}
