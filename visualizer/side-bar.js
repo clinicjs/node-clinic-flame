ef462b761584cef6128b1c94bd8f9df6774e558c
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-02-20 13:32:11 +0100

    sideBar responsive version

--- visualizer/side-bar.js
+++ visualizer/side-bar.js
@@ -1,6 +1,7 @@
 'use strict'
-
 const HtmlContent = require('./html-content.js')
+const button = require('./common/button.js')
+const close = require('@nearform/clinic-common/icons/close')
 
 class SideBar extends HtmlContent {
   constructor (parentContent, contentProperties = {}) {
@@ -11,13 +12,20 @@ class SideBar extends HtmlContent {
 
   initializeElements () {
     super.initializeElements()
+    this.d3ContentWrapper.append(() => button({
+      leftIcon: close,
+      classNames: ['side-bar-close-btn'],
+      onClick: () => {
+        this.ui.toggleSideBar(false)
+      }
+    }))
     this.d3ContentWrapper.attr('id', 'side-bar')
 
     this.d3ContentWrapper.on('animationend', () => {
       this.isExpanded = !this.isExpanded
 
       if (this.isExpanded) {
-        this.d3ContentWrapper.classed('slide-out', false)
+        this.d3ContentWrapper.classed('contract', false)
       }
 
       this.animationEnd(this.isExpanded)
@@ -25,18 +33,18 @@ class SideBar extends HtmlContent {
   }
 
   slideIn () {
-    this.d3ContentWrapper.classed('slide-in', true)
+    this.d3ContentWrapper.classed('expand', true)
   }
 
   slideOut () {
-    this.d3ContentWrapper.classed('slide-in', false)
-    this.d3ContentWrapper.classed('slide-out', true)
+    this.d3ContentWrapper.classed('expand', false)
+    this.d3ContentWrapper.classed('contract', true)
   }
 
   toggle (isVisible) {
-    // const className = isVisible ? 'slide-in' : 'slide-out'
-    this.d3ContentWrapper.classed('slide-in', isVisible)
-    this.d3ContentWrapper.classed('slide-out', !isVisible)
+    // const className = isVisible ? 'expand' : 'contract'
+    this.d3ContentWrapper.classed('expand', isVisible)
+    this.d3ContentWrapper.classed('contract', !isVisible)
   }
 
   draw () {
