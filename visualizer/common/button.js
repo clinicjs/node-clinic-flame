37070670c8a547793b439d7d46200b6a9531021a
Alessio Carnevale <cuginoAle@users.noreply.github.com>
2019-02-26 16:58:56 +0100

    Filters/footer redesign (#85)
    * replacing `More info` drop-down with a button
    
    * setting drop-down border colours - commenting out 2px border
    
    * fixing scroll-bar appearing on side-bar in

--- visualizer/common/button.js
+++ visualizer/common/button.js
@@ -1,7 +1,9 @@
-module.exports = ({ label, classNames = [], leftIcon = '', rightIcon = '', disabled = false, onClick } = {}) => {
+module.exports = ({ label, classNames = [], leftIcon = '', rightIcon = '', disabled = false, onClick, title } = {}) => {
   const button = document.createElement('button')
   button.classList.add('button', ...classNames)
+
   if (disabled) button.attr('disabled', true)
+  if (title) button.setAttribute('title', title)
 
   if (onClick) {
     button.addEventListener('click', onClick)
