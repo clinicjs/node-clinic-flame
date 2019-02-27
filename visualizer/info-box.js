2960035ab848bdeedeba9827987a721db658083f
Stuart Wilson <sjwilson89@gmail.com>
2019-02-27 14:28:23 +0000

    Flame graph percentage display (#74)
    * [621] - Implement flame graph percentages as dropdown alongside info box
    
    * [621] - Move percentage dropdown above info-box
    
    * [621] - Fix percentage dropdown button padding

--- visualizer/info-box.js
+++ visualizer/info-box.js
@@ -1,6 +1,7 @@
 'use strict'
 const HtmlContent = require('./html-content.js')
 const getNoDataNode = require('./no-data-node.js')
+const caretUpIcon = require('@nearform/clinic-common/icons/caret-up')
 
 class InfoBox extends HtmlContent {
   constructor (parentContent, contentProperties = {}) {
@@ -14,6 +15,16 @@ class InfoBox extends HtmlContent {
     this.functionText = functionName
     this.pathHtml = fileName
     this.areaText = 'Processing data...'
+    this.stackPercentages = {
+      top: 0,
+      overall: 0
+    }
+
+    this.addCollapseControl(true, {
+      classNames: 'frame-dropdown',
+      htmlElementType: 'button',
+      htmlContent: `<span>0%</span> ${caretUpIcon}`
+    })
   }
 
   initializeElements () {
@@ -35,6 +46,35 @@ class InfoBox extends HtmlContent {
     this.d3FrameArea = this.d3FrameInfo.append('span')
       .classed('frame-info-item', true)
       .classed('frame-area', true)
+
+    this.d3StackInfoTitle = this.d3ContentWrapper
+      .append('h2')
+      .text('Stack info')
+
+    this.d3StackPercentageTop = this.d3ContentWrapper
+      .append('p')
+      .classed('frame-percentage', true)
+      .classed('frame-percentage-top', true)
+      .text('0%')
+
+    this.d3StackPercentageOverall = this.d3ContentWrapper
+      .append('p')
+      .classed('frame-percentage', true)
+      .classed('frame-percentage-overall', true)
+      .text('0%')
+
+    this.d3CollapseButton = this.collapseControl.d3Element
+      .attr('title', 'Show stack info')
+
+    // Close when the user clicks outside the options menu.
+    document.body.addEventListener('click', (event) => {
+      if (!this.collapseClose.isCollapsed &&
+          !this.d3CollapseButton.node().contains(event.target) &&
+          !this.d3ContentWrapper.node().contains(event.target)) {
+        this.collapseClose()
+      }
+    },
+    true)
   }
 
   contentFromNode (node) {
@@ -43,12 +83,20 @@ class InfoBox extends HtmlContent {
       return
     }
 
+    // Todo: Use visibleRootValue when ready
+    const totalValue = this.ui.dataTree.activeTree().value
+
+    this.stackPercentages = {
+      top: Math.round(100 * (node.onStackTop.asViewed / totalValue) * 10) / 10,
+      overall: Math.round(100 * (node.value / totalValue) * 10) / 10
+    }
+
     this.functionText = node.functionName
 
-    this.pathHtml = node.fileName
+    this.pathHtml = node.fileName || ''
     if (node.lineNumber && node.columnNumber) {
       // Two spaces (in <pre> tag) so this is visually linked to but distinct from main path, including when wrapped
-      this.pathHtml += `  <span class="frame-line-col">line:${node.lineNumber} column:${node.columnNumber}</span>`
+      this.pathHtml += `<span class="frame-line-col"><span>  line</span>:${node.lineNumber}<span> column</span>:${node.columnNumber}</span>`
     }
 
     this.rankNumber = this.ui.dataTree.getSortPosition(node)
@@ -75,9 +123,12 @@ class InfoBox extends HtmlContent {
   draw () {
     super.draw()
 
-    this.d3FrameFunction.text(this.functionText)
-    this.d3FramePath.html(this.pathHtml)
-    this.d3FrameArea.text(this.areaText)
+    this.d3FrameFunction.text(this.functionText).attr('title', this.functionText)
+    this.d3FramePath.html(this.pathHtml).attr('title', this.pathHtml.replace(/(<([^>]+)>)/ig, ''))
+    this.d3FrameArea.text(this.areaText).attr('title', this.areaText)
+    this.d3CollapseButton.select('span').text(`${this.stackPercentages.top}%`)
+    this.d3StackPercentageTop.text(`Top of stack: ${this.stackPercentages.top}%`)
+    this.d3StackPercentageOverall.text(`On stack: ${this.stackPercentages.overall}%`)
   }
 }
 
