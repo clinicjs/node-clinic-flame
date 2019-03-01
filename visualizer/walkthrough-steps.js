f6b730bc5fe6000824bfafa7a7fdcf5e0cd1b85d
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-03-02 00:30:56 +0100

    defining some demo steps

--- visualizer/walkthrough-steps.js
+++ visualizer/walkthrough-steps.js
@@ -1,20 +1,39 @@
-
 const WalkthroughSteps = [
   {
     attachTo: '#selection-controls :nth-child(4)',
-    msg: `Hello, and welcome to this amazing App!`
+    msg: `
+    <div class="step-1">
+      <svg style="width:40px" class="Icon__Svg-sc-1vaf90b-0 ekOBdq" viewBox="0 0 64 64" fill="#ffaa2b"><path d="M61.23,21.21A8.58,8.58,0,0,0,57,14.75C49.93,10.42,43,6,36.45,2a6.38,6.38,0,0,0-6.9,0C23,6,16.07,10.45,9,14.75a8.63,8.63,0,0,0-4.21,6.46,165.32,165.32,0,0,0,0,21.58A8.58,8.58,0,0,0,9,49.25C16.07,53.55,23,58,29.55,62a6.38,6.38,0,0,0,6.9,0C43,58,49.93,53.55,57,49.25a8.5,8.5,0,0,0,4.21-6.46A165.32,165.32,0,0,0,61.23,21.21Zm-21-2.77H42.2a1.45,1.45,0,1,1,0,2.9H40.26a1.45,1.45,0,1,1,0-2.9Zm0,5.81h5.82a1.46,1.46,0,0,1,0,2.91H40.26a1.46,1.46,0,0,1,0-2.91Zm0,5.81h5.82a1.46,1.46,0,0,1,0,2.91H40.26a1.46,1.46,0,0,1,0-2.91Zm0,5.81H49a1.46,1.46,0,1,1,0,2.91H40.26a1.46,1.46,0,0,1,0-2.91ZM16.05,24.25h3.87a1.46,1.46,0,0,1,0,2.91H16.05a1.46,1.46,0,1,1,0-2.91Zm0,5.81H27.67a1.46,1.46,0,0,1,0,2.91H16.05a1.46,1.46,0,1,1,0-2.91Zm0,5.81H32.51a1.46,1.46,0,1,1,0,2.91H16.05a1.46,1.46,0,1,1,0-2.91ZM49,44.59H16.05a1.45,1.45,0,1,1,0-2.9H49a1.45,1.45,0,1,1,0,2.9Z"></path></svg>    
+      Hello, and welcome to Flame!
+    </div>`
   },
   {
     attachTo: '.frame-dropdown',
-    msg: `<div class="">Hello, and welcome to this amazing App!</div>`
-  },
-  {
-    attachTo: '#filters-bar .left-col',
-    msg: `<div class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>`
+    msg: `
+    <div>
+      <p>Do you like Flame?</p>
+      <img style="width:400px; height: 225px; display:block;" src="https://images.pexels.com/photos/750225/pexels-photo-750225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+    </div>`
   },
   {
     attachTo: '#filters-bar .key-app',
-    msg: `<div class="">Hello, and welcome to this amazing App!</div>`
+    msg: `
+    <div>
+      <p>This is just another example :)</p>
+      <div class="scrollable scroll-container">
+        <p>Some looong content</p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <img style="width:300px; height: 225px; display:block;" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/cdba9817085171.562b56f8b69cd.gif" />
+      </div>
+    </div>`
+  },
+  {
+    attachTo: '#filters-bar .left-col',
+    msg: `
+    <div class="">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <iframe width="437" height="246" src="https://www.youtube.com/embed/KvVCafGmrWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>`
   }
 ]
 
