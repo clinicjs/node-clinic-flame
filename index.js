aa5d3de19d403dfdc5a37051d8df6aa29b08dd00
Stuart Wilson <stuart.wilson@nearform.com>
2019-03-12 11:29:28 +0000

    [596] - Remove bodyClass that was hiding loader

--- index.js
+++ index.js
@@ -150,7 +150,6 @@ class ClinicFlame extends events.EventEmitter {
       headerText: 'Flame',
       nearFormLogo: nearFormLogoFile,
       uploadId: outputFilename.split('/').pop().split('.html').shift(),
-      bodyClass: 'has-no-spinner is-loading-font',
       body: '<main></main>'
     })
 
