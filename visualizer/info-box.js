f92b5752008c41146194f4c52a3c20a149c48b30
Renée Kooi <renee@kooi.me>
2019-10-29 11:13:32 +0100

    Info box responsive improvements (#104)
    * [621] - Create method in DataTree to count total frames from source data and use show percentage on stack top each frame is in the UI
    
    * [621] - Implement dropdown with top and overall stack percentage into info-box
    
    * [621] - Tidy info-box UI
    
    * [621] - Use active tree value to calculate frame percentage
    
    * [621] - Fix selection-controls margin
    
    * [621] - Optimise text display in info box. Flag future use of visibleRootValue for stack percentages
    
    * [621] - Fix type error when generating path title attribute
    
    * [621] - Revert changes to text arrangement in info-box
    
    * [621] - Ensure line and col numbers are spaced properly
    
    * [621] - Use pointer cursor on percentage dropdown button
    
    * [621] - Ensure percentage dropdown closes on click outside and remove empty property from DataTree
    
    * [621] - Create method in DataTree to count total frames from source data and use show percentage on stack top each frame is in the UI
    
    * [621] - Use active tree value to calculate frame percentage
    
    * [621] - Tweak responsive nature of info-box text
    
    * [621] - Make more sections of info text responsive and rely on labels for full text content where necessary
    
    * [621] - Tweak styling to find line of best fit between different outputs, function name lengths etc
    
    * Use tooltip component.
    
    * make frame info tooltip full-width on mobile, fix some overflows

--- visualizer/info-box.js
+++ visualizer/info-box.js
@@ -1,10 +1,23 @@
 'use strict'
+const d3 = require('./d3.js')
 const HtmlContent = require('./html-content.js')
 const getNoDataNode = require('./no-data-node.js')
+const caretUpIcon = require('@nearform/clinic-common/icons/caret-up')
+
+const stripTags = html => html.replace(/(<([^>]+)>)/ig, '')
+
+const addResponsiveSpan = str => `<span class="visible-md">${str}</span>`
+
+const wrapTooltipText = text =>
+  d3.create('span')
+    .classed('tooltip-default-message frame-tooltip', true)
+    .text(text)
+    .node()
 
 class InfoBox extends HtmlContent {
   constructor (parentContent, contentProperties = {}) {
     super(parentContent, contentProperties)
+    this.tooltip = contentProperties.customTooltip
 
     const {
       functionName,
@@ -13,32 +26,79 @@ class InfoBox extends HtmlContent {
 
     this.functionText = functionName
     this.pathHtml = fileName
-    this.areaText = 'Processing data...'
+    this.areaHtml = 'Processing data...'
     this.stackPercentages = {
       top: 0,
       overall: 0
     }
+
+    this.addCollapseControl(true, {
+      classNames: 'frame-dropdown',
+      htmlElementType: 'button',
+      htmlContent: `<span>0%</span> ${caretUpIcon}`
+    })
   }
 
   initializeElements () {
     super.initializeElements()
 
     // Initialize frame info
-    this.d3FrameInfo = this.d3Element.append('pre')
+    this.d3FrameInfo = this.d3Element.append('div')
       .classed('frame-info', true)
       .classed('panel', true)
 
     this.d3FrameFunction = this.d3FrameInfo.append('strong')
       .classed('frame-info-item', true)
       .classed('frame-function', true)
+    this.tooltip.attach({
+      msg: () => wrapTooltipText(this.functionText),
+      d3TargetElement: this.d3FrameFunction
+    })
 
     this.d3FramePath = this.d3FrameInfo.append('span')
       .classed('frame-info-item', true)
       .classed('frame-path', true)
+    this.tooltip.attach({
+      msg: () => wrapTooltipText(stripTags(this.pathHtml)),
+      d3TargetElement: this.d3FramePath
+    })
 
     this.d3FrameArea = this.d3FrameInfo.append('span')
       .classed('frame-info-item', true)
       .classed('frame-area', true)
+    this.tooltip.attach({
+      msg: () => wrapTooltipText(stripTags(this.areaHtml)),
+      d3TargetElement: this.d3FrameArea
+    })
+
+    this.d3StackInfoTitle = this.d3ContentWrapper
+      .append('h2')
+      .text('Stack info')
+
+    this.d3StackPercentageTop = this.d3ContentWrapper
+      .append('p')
+      .classed('frame-percentage', true)
+      .classed('frame-percentage-top', true)
+      .text('0%')
+
+    this.d3StackPercentageOverall = this.d3ContentWrapper
+      .append('p')
+      .classed('frame-percentage', true)
+      .classed('frame-percentage-overall', true)
+      .text('0%')
+
+    this.d3CollapseButton = this.collapseControl.d3Element
+      .attr('title', 'Show stack info')
+
+    // Close when the user clicks outside the options menu.
+    document.body.addEventListener('click', (event) => {
+      if (!this.collapseClose.isCollapsed &&
+          !this.d3CollapseButton.node().contains(event.target) &&
+          !this.d3ContentWrapper.node().contains(event.target)) {
+        this.collapseClose()
+      }
+    },
+    true)
   }
 
   contentFromNode (node) {
@@ -56,11 +116,19 @@ class InfoBox extends HtmlContent {
     }
 
     this.functionText = node.functionName
+    this.pathHtml = ''
 
-    this.pathHtml = node.fileName || ''
+    if (node.fileName) {
+      const fileNameParts = (node.fileName || '').split('/')
+      const baseName = fileNameParts.pop()
+      const prefix = fileNameParts.join('/')
+
+      this.pathHtml = `${addResponsiveSpan(`${prefix}/`)}${baseName}`
+    }
+
     if (node.lineNumber && node.columnNumber) {
       // Two spaces (in <pre> tag) so this is visually linked to but distinct from main path, including when wrapped
-      this.pathHtml += `<span class="frame-line-col"><span>  line</span>:${node.lineNumber}<span> column</span>:${node.columnNumber}</span>`
+      this.pathHtml += `<span class="frame-line-col">${addResponsiveSpan('  line')}:${node.lineNumber}${addResponsiveSpan(' column')}:${node.columnNumber}</span>`
     }
 
     this.rankNumber = this.ui.dataTree.getSortPosition(node)
@@ -68,14 +136,23 @@ class InfoBox extends HtmlContent {
     const typeLabel = node.category === 'core' ? '' : ` (${this.ui.getLabelFromKey(`${node.category}:${node.type}`, true)})`
     const categoryLabel = this.ui.getLabelFromKey(node.category, true)
 
-    // e.g. The no-data-node has an .areaText containing a custom message
-    this.areaText = node.areaText || `In ${categoryLabel}${typeLabel}`
+    this.areaHtmlColour = this.ui.getFrameColor(
+      {
+        category: node.category
+      },
+      'foreground',
+      false
+    )
 
-    if (node.isInit) this.areaText += '. In initialization process'
-    if (node.isInlinable) this.areaText += '. Inlinable'
-    if (node.isUnoptimized) this.areaText += '. Unoptimized'
-    if (node.isOptimized) this.areaText += '. Optimized'
-    this.areaText += '.'
+    // e.g. The no-data-node has an .areaText containing a custom message
+    this.areaHtml = node.areaText || `${addResponsiveSpan(`In ${categoryLabel} `)}${typeLabel}`
+
+    if (node.isInit) this.areaHtml += '. In initialization process'
+    if (node.isInlinable) this.areaHtml += '. Inlinable'
+    if (node.isUnoptimized) this.areaHtml += '. Unoptimized'
+    if (node.isOptimized) this.areaHtml += '. Optimized'
+
+    this.areaHtml += addResponsiveSpan('.')
 
     this.draw()
   }
@@ -87,9 +164,25 @@ class InfoBox extends HtmlContent {
   draw () {
     super.draw()
 
-    this.d3FrameFunction.text(this.functionText).attr('title', this.functionText)
-    this.d3FramePath.html(this.pathHtml).attr('title', this.pathHtml.replace(/(<([^>]+)>)/ig, ''))
-    this.d3FrameArea.text(this.areaText).attr('title', this.areaText)
+    this.d3FrameFunction
+      .text(this.functionText)
+
+    this.d3FramePath
+      .html(this.pathHtml)
+
+    this.d3FrameArea
+      .html(this.areaHtml)
+      .style('color', this.areaHtmlColour)
+
+    this.d3CollapseButton
+      .select('span')
+      .text(`${this.stackPercentages.top}%`)
+
+    this.d3StackPercentageTop
+      .text(`Top of stack: ${this.stackPercentages.top}%`)
+
+    this.d3StackPercentageOverall
+      .text(`On stack: ${this.stackPercentages.overall}%`)
   }
 }
 
