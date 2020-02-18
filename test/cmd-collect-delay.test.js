f0d4b9d4f69c5353ba1e640b7b9c5a4f298dc0db
Dylan Coakley <dylan.coakley@nearform.com>
2020-02-18 16:19:56 +0000

    Add file to test the new collect delay feature

---
+++ test/cmd-collect-delay.test.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const { test } = require('tap')
+const rimraf = require('rimraf')
+const ClinicFlame = require('../index.js')
+
+test('cmd - test collect - 1s collect delay', (t) => {
+  const tool = new ClinicFlame({ debug: true, collectDelay: 1000 })
+
+  function delayOneSecond() {
+    setTimeout(() => {
+      console.log('return after one second')
+      return false;
+    }, 1000);
+  }
+
+  function cleanup (err, dirname) {
+    t.ifError(err)
+    t.match(dirname, /^test-output-destination[/\\][0-9]+\.clinic-flame$/)
+
+    rimraf('test-output-destination', (err) => {
+      t.ifError(err)
+      t.end()
+    })
+  }
+
+  tool.collect(
+    [process.execPath, path.join('test', 'fixtures', 'inspect.js')],
+    function (err, dirname) {
+      if (err) return cleanup(err, dirname)
+
+      const getLoggingPaths = require('@nearform/clinic-common').getLoggingPaths('flame')
+      const analyse = require('../analysis')
+      const paths = getLoggingPaths({ path: dirname })
+      analyse(paths).then((result) => {
+        console.log(result)
+      })
+
+      cleanup(null, dirname)
+    }
+  )
+})
