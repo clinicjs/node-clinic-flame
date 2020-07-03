482c382577b7892cbfc3994cbe846541f0f9eda7
Adam Davis <adam@admataz.com>
2020-07-03 18:16:36 +0100

    t.jobs=2 for wasm test

--- test/basic.test.js
+++ test/basic.test.js
@@ -81,7 +81,6 @@ test('cmd - test visualization - missing data', function (t) {
 })
 
 test('cmd - test collect - system info, data files and html', function (t) {
-  t.jobs = 1
   const tool = new ClinicFlame()
 
   function cleanup (err, dirname) {
@@ -111,6 +110,7 @@ test('cmd - test collect - system info, data files and html', function (t) {
 })
 
 test('cmd - test collect - does not crash on webassembly frames', function (t) {
+  t.jobs = 2
   const tool = new ClinicFlame()
 
   function cleanup (err, dirname) {
